bc6f916edf39051083673937114d8c13e6075806
TheRoux04 <1908527@etu.cegep-lanaudiere.qc.ca>
2020-01-24 11:38:34 -0500

    ajout

--- Tableaux.java
+++ Tableaux.java
@@ -39,4 +39,153 @@ public class Tableaux {
                 permute(tab, i, imin);
         }
     }
+
+    public static void trierDecroissantSSS(int[] tab){
+        int imax;
+        for (int i=0; i<tab.length-1; i++){
+            imax = i;
+            for (int j=i+1; j<tab.length; j++){
+                if (tab[j] > tab[imax])
+                    imax = j;
+            }
+            if (imax != i)
+                permute(tab, i, imax);
+        }
+    }
+
+    public static void print(int[] tab){
+        for (int elem: tab) {
+            System.out.print(elem + ", ");
+        }
+    }
+
+    public static int maximum(int[] tab){
+        int imax = tab[0];
+        for (int i = 0; i < tab.length; i++){
+            if (tab[i] > imax){
+                imax = tab[i];
+            }
+        }
+        return imax;
+    }
+
+    public static int minimum(int[] tab){
+        int imin = tab[0];
+        for (int i = 0; i < tab.length; i++){
+            if (tab[i] < imin){
+                imin = tab[i];
+            }
+        }
+        return imin;
+    }
+
+    public static double moyenne(int[] tab){
+        int addition = 0;
+        double moyenne;
+        for (int i = 0; i < tab.length; i++){
+            addition += tab[i];
+        }
+        moyenne = addition / tab.length;
+
+        if (moyenne != 0){
+            return moyenne;
+        }
+        else{
+            return -1;
+        }
+    }
+
+    public static int fouilleSeq(int[] tab, int rechercher){
+        boolean trouve = false;
+        int i = 0;
+        while (!trouve && i < tab.length){
+            if (tab[i] == rechercher){
+                trouve = true;
+            }
+            else{
+                i++;
+            }
+        }
+
+        if (trouve){
+            return i;
+        }
+        else{
+            return -1;
+        }
+    }
+
+    public static void trierDecroissantSSS(String[] tab){
+        int imax;
+        for (int i=0; i<tab.length-1; i++){
+            imax = i;
+            for (int j=i+1; j<tab.length; j++){
+                if (Integer.parseInt(tab[j]) > Integer.parseInt(tab[imax]))
+                    imax = j;
+            }
+            if (imax != i)
+                permute(tab, i, imax);
+        }
+    }
+
+    public static void print(String[] tab){
+        for (String elem: tab) {
+            System.out.print(elem + ", ");
+        }
+    }
+
+    public static int maximum(String[] tab){
+        int imax = Integer.parseInt(tab[1]);
+        for (int i = 0; i < tab.length; i++){
+            if (Integer.parseInt(tab[i]) > imax){
+                imax = Integer.parseInt(tab[i]);
+            }
+        }
+        return imax;
+    }
+
+    public static int minimum(String[] tab){
+        int imin = Integer.parseInt(tab[1]);
+        for (int i = 0; i < tab.length; i++){
+            if (Integer.parseInt(tab[i]) < imin){
+                imin = Integer.parseInt(tab[i]);
+            }
+        }
+        return imin;
+    }
+
+    public static double moyenne(String[] tab){
+        int addition = 0;
+        double moyenne;
+        for (int i = 0; i < tab.length; i++){
+            addition += Integer.parseInt(tab[i]);
+        }
+        moyenne = addition / tab.length;
+
+        if (moyenne != 0){
+            return moyenne;
+        }
+        else{
+            return -1;
+        }
+    }
+
+    public static int fouilleSeq(String[] tab, int rechercher) {
+        boolean trouve = false;
+        int i = 0;
+        while (!trouve && i < tab.length) {
+            if (Integer.parseInt(tab[i]) == rechercher) {
+                trouve = true;
+            }
+            else{
+                i++;
+            }
+        }
+
+        if (trouve) {
+            return i;
+        } else {
+            return -1;
+        }
+    }
 }
